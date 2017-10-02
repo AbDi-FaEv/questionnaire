eaaba2724a4b20917560ec6442cfbc6b6bb83e50
Dmitry Abramenko <abramenko@a2design.biz>
2017-10-02 17:54:46 +0600

    chore (migrations) add role id to users table

---
+++ database/migrations/2017_10_02_115047_add_role_id_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoleIdUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedInteger('role_id')->after('password');
+            $table->foreign('role_id')
+                ->references('id')->on('roles')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id'])->after('password');
+            $table->dropColumn('role_id')->after('password');
+        });
+    }
+}
