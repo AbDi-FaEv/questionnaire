94664418200efa7e116137a2357306472da0a03d
Dmitry Abramenko <abramenko@a2design.biz>
2017-10-02 18:14:03 +0600

    chore (views) add custom views for server errors

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="wrap">
+	<div class="errors">
+		<h2 class="text-center">403</h2>
+		<h3 class="text-center">ACCESS FORBIDDEN</h3>
+	</div>
+</div>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="wrap">
+	<div class="errors">
+		<h2 class="text-center">500</h2>
+		<h3 class="text-center">INTERNAL ERROR</h3>
+	</div>
+</div>
+@endsection
